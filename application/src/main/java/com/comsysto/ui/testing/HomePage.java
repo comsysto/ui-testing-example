588508f51459c318e9c249c913763a4c6086703c
Elisabeth Engel <elisabeth.engel@comsysto.com>
2015-04-01 19:22:27 +0200

    [IMP] improving the example application

--- application/src/main/java/com/comsysto/ui/testing/HomePage.java
+++ application/src/main/java/com/comsysto/ui/testing/HomePage.java
@@ -13,24 +13,31 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 
+	private int slowmotion;
+
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
+
+		slowmotion = parameters.get("slowmotion").toInt(0);
+
+		slowmotion();
 
 		IModel<TimeSaving> timeSavingModel = Model.of(new TimeSaving(5,1000));
 
 		setDefaultModel(timeSavingModel);
 
-		final TextField<Integer> timePerUser = new TextField<>("timePerUser", new PropertyModel<>(getDefaultModel(), "timePerUser"));
-		final TextField<Integer> amountUsers = new TextField<>("amountUsers", new PropertyModel<>(getDefaultModel(), "amountUsers"));
 		final Label resultInSecondsPerDay = new Label("resultInSecondsPerDay", new PropertyModel<>(getDefaultModel(), "resultInSecondsPerDay"));
 		final Label resultInSecondsPerYear = new Label("resultInSecondsPerYear", new PropertyModel<>(getDefaultModel(), "resultInSecondsPerYear"));
 		final WebMarkupContainer results = new WebMarkupContainer("results");
 		results.setVisible(false);
 		add(results);
+		results.add(resultInSecondsPerDay);
+		results.add(resultInSecondsPerYear);
 
 		Form form = new Form("form") {
 			@Override
 			protected void onSubmit() {
+				slowmotion();
 				timeSavingModel.getObject().calculateAndSetTimeSaving();
 				results.setVisible(true);
 			}
@@ -38,11 +45,20 @@ public class HomePage extends WebPage {
 
 		add(form);
 
+		final TextField<Integer> timePerUser = new TextField<>("timePerUser", new PropertyModel<>(getDefaultModel(), "timePerUser"));
 		form.add(timePerUser);
+		final TextField<Integer> amountUsers = new TextField<>("amountUsers", new PropertyModel<>(getDefaultModel(), "amountUsers"));
 		form.add(amountUsers);
 		form.add(new SubmitLink("submit"));
-
-		results.add(resultInSecondsPerDay);
-		results.add(resultInSecondsPerYear);
     }
+
+	private void slowmotion () {
+		if (slowmotion > 0) {
+			try {
+				Thread.sleep(slowmotion * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
